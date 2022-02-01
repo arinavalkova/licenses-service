c47ec05a9f01e1bcb80e7af0786d494c04978a50
Arina <sunny.qww@list.ru>
2022-02-01 12:42:27 +0700

    Initial commit

---
+++ src/main/java/ru/shift/baldezh/licenses/service/LicensesServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.shift.baldezh.licenses.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shift/baldezh/licenses/service/LicensesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shift.baldezh.licenses.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
