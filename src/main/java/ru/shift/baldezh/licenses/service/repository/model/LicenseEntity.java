ff06e8c6969bbc166da8259f01107a3a356367d0
Arina <sunny.qww@list.ru>
2022-02-02 14:27:17 +0700

    added fields to license entity

--- src/main/java/ru/shift/baldezh/licenses/service/repository/model/LicenseEntity.java
+++ src/main/java/ru/shift/baldezh/licenses/service/repository/model/LicenseEntity.java
@@ -1,5 +1,6 @@
 package ru.shift.baldezh.licenses.service.repository.model;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Date;
@@ -9,10 +10,15 @@ public class LicenseEntity {
     @GeneratedValue
     private long licenseId;
 
+    @Column(name = "creation_date", nullable = false)
     private Date creationDate;
 
+    @Column(name = "expiration_date", nullable = false)
     private Date expirationDate;
 
+    @Column(name = "sign", nullable = false)
+    private String sign;
 
-
+    @Column(name = "mail", nullable = false)
+    private String mail;
 }
