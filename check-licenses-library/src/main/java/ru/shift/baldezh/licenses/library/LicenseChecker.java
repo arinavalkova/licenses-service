27792ee5362948ea07c71289d2f619d0532f4bde
Arina <sunny.qww@list.ru>
2022-02-04 15:41:31 +0700

    added findCheckLicenseForm method to LicenseProduct

--- check-licenses-library/src/main/java/ru/shift/baldezh/licenses/library/LicenseChecker.java
+++ check-licenses-library/src/main/java/ru/shift/baldezh/licenses/library/LicenseChecker.java
@@ -1,11 +1,12 @@
 package ru.shift.baldezh.licenses.library;
 
-import java.io.File;
+import ru.shift.baldezh.licenses.service.model.forms.license.CheckLicenseForm;
+
 import java.security.PublicKey;
 
 public class LicenseChecker {
 
-    public boolean isActivated(PublicKey publicKey, File license, String uniqueHardwareId) {
+    public boolean isActivated(PublicKey publicKey, CheckLicenseForm checkLicenseForm, String uniqueHardwareId) {
         return true;
     }
 }
--- check-licenses-library/src/main/java/ru/shift/baldezh/licenses/product/LicenseProduct.java
+++ check-licenses-library/src/main/java/ru/shift/baldezh/licenses/product/LicenseProduct.java
@@ -1,12 +1,13 @@
 package ru.shift.baldezh.licenses.product;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.shift.baldezh.licenses.library.LicenseChecker;
+import ru.shift.baldezh.licenses.service.model.forms.license.CheckLicenseForm;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
@@ -16,35 +17,44 @@ import java.security.spec.X509EncodedKeySpec;
 
 public class LicenseProduct {
 
-    private static String CRYPTO_ALGORITHM = "RSA";
-    private final static String PUBLIC_FILE_NAME = "public.key";
+    private static final String CRYPTO_ALGORITHM = "RSA";
+    private static final String PUBLIC_FILE_NAME = "public.key";
+    private static final String LICENSE_FILE_NAME = "license.json";
+
+
     private static PublicKey publicKey;
     private static String uniqueHardwareId;
-    private static File license;
+    private static CheckLicenseForm checkLicenseForm;
 
     public static void main(String[] args) {
-        if (isLicenseValid()) {
+        if (isLicenseValid(args)) {
             work();
         } else {
             buy();
         }
     }
 
-    private static boolean isLicenseValid() {
+    private static boolean isLicenseValid(String[] args) {
         try {
-            publicKey = getPublicKey();
-            //here getting publicKey from file
-            //here getting uniqueHardwareId
-            //here getting license from file
+            publicKey = findPublicKey();
+            uniqueHardwareId = args[0];
+            checkLicenseForm = findCheckLicenseForm();
+
             LicenseChecker licenseChecker = new LicenseChecker();
-            return licenseChecker.isActivated(publicKey, license, uniqueHardwareId);
+            return licenseChecker.isActivated(publicKey, checkLicenseForm, uniqueHardwareId);
+
         } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    private static PublicKey getPublicKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    private static CheckLicenseForm findCheckLicenseForm() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(Paths.get(LICENSE_FILE_NAME).toFile(), CheckLicenseForm.class);
+    }
+
+    private static PublicKey findPublicKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
         File publicKeyFile = new File(PUBLIC_FILE_NAME);
         byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
         EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
