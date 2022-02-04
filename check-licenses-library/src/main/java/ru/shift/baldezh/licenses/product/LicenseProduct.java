46e363740f4da13e01ad8a155f04e64263102fa1
Arina <sunny.qww@list.ru>
2022-02-04 15:18:43 +0700

    added getPublicKey method from public.key file to LicenseProduct

--- check-licenses-library/src/main/java/ru/shift/baldezh/licenses/product/LicenseProduct.java
+++ check-licenses-library/src/main/java/ru/shift/baldezh/licenses/product/LicenseProduct.java
@@ -1,12 +1,22 @@
 package ru.shift.baldezh.licenses.product;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import ru.shift.baldezh.licenses.library.LicenseChecker;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
 public class LicenseProduct {
 
+    private static String CRYPTO_ALGORITHM = "RSA";
     private final static String PUBLIC_FILE_NAME = "public.key";
     private static PublicKey publicKey;
     private static String uniqueHardwareId;
@@ -21,15 +31,25 @@ public class LicenseProduct {
     }
 
     private static boolean isLicenseValid() {
-        //here getting publicKey from file
-        //here getting uniqueHardwareId
-        //here getting license from file
-        LicenseChecker licenseChecker = new LicenseChecker();
-        return licenseChecker.isActivated(publicKey, license, uniqueHardwareId);
+        try {
+            publicKey = getPublicKey();
+            //here getting publicKey from file
+            //here getting uniqueHardwareId
+            //here getting license from file
+            LicenseChecker licenseChecker = new LicenseChecker();
+            return licenseChecker.isActivated(publicKey, license, uniqueHardwareId);
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    private PublicKey getPublicKey() {
-
+    private static PublicKey getPublicKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        File publicKeyFile = new File(PUBLIC_FILE_NAME);
+        byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
+        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(CRYPTO_ALGORITHM);
+        return keyFactory.generatePublic(publicKeySpec);
     }
 
     private static void work() {
